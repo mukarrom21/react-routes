61fd4a0ad6ba63038bbd897beef9aa20b27710d0
mukarrom21 <mukarrom21@gmail.com>
2022-03-29 20:15:08 +0600

    many things

---
+++ src/components/About/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h2>Wanna to know about us?</h2>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Welcome to my Fancy Routing !!!</h1>
+      <nav>
+          <a href="/">Home</a>
+          <a href="/friends">Friends</a>
+          <a href="/about">About</a>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
